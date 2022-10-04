a966f71693268de7401a345d01c69712f0f1d538
yuxxeun <itsyuxxeun@gmail.com>
2022-10-05 06:48:47 +0700

    styles: kosmetik

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,36 +7,34 @@ import image from '@astrojs/image';
 import partytown from '@astrojs/partytown';
 import { SITE } from './src/config.mjs';
 import prefetch from '@astrojs/prefetch';
+import svelte from "@astrojs/svelte";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+
+// https://astro.build/config
 export default defineConfig({
-	site: SITE.origin,
-	base: SITE.basePathname,
-	output: 'static',
-	integrations: [
-		tailwind({
-			config: {
-				applyBaseStyles: false,
-			},
-		}),
-		sitemap(),
-		image(),
-		/* Disable this integration if you don't use Google Analytics (or other external script). */
-		partytown({
-			config: {
-				forward: ['dataLayer.push'],
-			},
-		}),
-		prefetch({
-			selector: "a[href^='/']",
-		}),
-	],
-	vite: {
-		resolve: {
-			alias: {
-				'~': path.resolve(__dirname, './src'),
-			},
-		},
-	},
+  site: SITE.origin,
+  base: SITE.basePathname,
+  output: 'static',
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), sitemap(), image(),
+  /* Disable this integration if you don't use Google Analytics (or other external script). */
+  partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  }), prefetch({
+    selector: "a[href^='/']"
+  }), svelte()],
+  vite: {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src')
+      }
+    }
+  }
 });
