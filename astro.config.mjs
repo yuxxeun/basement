52b5083aa2f858fac9dd2ea518a43a34adad5460
yuxxeun <itsyuxxeun@gmail.com>
2022-11-23 13:46:35 +0700

    deps: vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,46 +1,42 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import sitemap from '@astrojs/sitemap'
-import image from '@astrojs/image'
-import partytown from '@astrojs/partytown'
-import { SITE } from './src/config.mjs'
-import prefetch from '@astrojs/prefetch'
-import svelte from '@astrojs/svelte'
-import vercel from '@astrojs/vercel/static'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import image from '@astrojs/image';
+import partytown from '@astrojs/partytown';
+import { SITE } from './src/config.mjs';
+import prefetch from '@astrojs/prefetch';
+import svelte from '@astrojs/svelte';
+import vercel from '@astrojs/vercel/static';
+import vercel from "@astrojs/vercel/serverless";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-	site: SITE.origin,
-	base: SITE.basePathname,
-	output: 'static',
-	integrations: [
-		tailwind({
-			config: {
-				applyBaseStyles: false,
-			},
-		}),
-		sitemap(),
-		image(),
-		/* Disable this integration if you don't use Google Analytics (or other external script). */
-		partytown({
-			config: {
-				forward: ['dataLayer.push'],
-			},
-		}),
-		prefetch({
-			selector: "a[href^='/']",
-		}),
-		svelte(),
-	],
-	vite: {
-		resolve: {
-			alias: {
-				'~': path.resolve(__dirname, './src'),
-			},
-		},
-	},
-})
+  site: SITE.origin,
+  base: SITE.basePathname,
+  output: 'static',
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), sitemap(), image(), /* Disable this integration if you don't use Google Analytics (or other external script). */
+  partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  }), prefetch({
+    selector: "a[href^='/']"
+  }), svelte()],
+  vite: {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src')
+      }
+    }
+  },
+  adapter: vercel()
+});
