01c1b594caef6137337d5f69e0f482b0fc92fcfe
yuxxeun <itsyuxxeun@gmail.com>
2023-03-13 14:21:45 +0700

    ref: supabase lib

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
-const supabaseKey = import.meta.env.PUBLIC_SUPABASE_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(import.meta.env.PUBLIC_SUPABASE_URL, import.meta.env.PUBLIC_SUPABASE_KEY)
