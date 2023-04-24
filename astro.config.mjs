5f15eef661fe17fadf4029cb295be6c905f0d97d
yuxxeun <itsyuxxeun@gmail.com>
2023-04-24 20:13:42 +0700

    ref(components): upadate hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import partytown from '@astrojs/partytown'
 import prefetch from '@astrojs/prefetch'
 import svelte from '@astrojs/svelte'
 import { vitePreprocess } from '@astrojs/svelte'
-import vercel from "@astrojs/vercel/edge"
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
