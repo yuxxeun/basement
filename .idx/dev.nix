455daebfd501d96d01d382b6812e81d27fdb6f69
kridoveronique <kridoveronique@gmail.com>
2024-06-04 07:14:30 +0000

    add idx integration

---
+++ .idx/dev.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }: {
+  
+  channel = "stable-23.11";
+
+  packages = [
+    pkgs.nodejs_20
+  ];
+
+  idx.previews = {
+    enable = true;
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "start"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+          "--disable-host-check"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
